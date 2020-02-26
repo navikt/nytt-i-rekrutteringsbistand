cd9963d2499d1d3c00cf9c6ca189580d8c5e5660
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-02-26 07:56:55 +0100

    Legg til nyhet 'Endring av fornavn og etternavn'

--- src/nytt/NyttIRekrutteringsbistand.tsx
+++ src/nytt/NyttIRekrutteringsbistand.tsx
@@ -6,9 +6,16 @@ const tilDato = (dag: number, måned: number, år: number) => new Date(år, mån
 
 const nyheter = [
     {
-        dato: tilDato(23, 2, 2020),
-        tittel: 'Vi har nå lansert endringsloggen!',
-        innhold: 'Her vil du få oversikt over nyheter og endringer i Rekrutteringsbistand.'
+        dato: tilDato(26, 2, 2020),
+        tittel: 'Endring av fornavn og etternavn',
+        innhold:
+            'I kandidatsøket vises nå etternavn før fornavn. På e-poster som automatisk sendes til arbeidsgiver har vi også endret rekkefølge. Nå vil fornavnet ditt vises før etternavnet.'
+    },
+    {
+        dato: tilDato(26, 2, 2020),
+        tittel: 'Vi har nå lansert «nytt i Rekrutteringsbistand»',
+        innhold:
+            'Her vil du få oversikt over nyheter og endringer som gjøres. Du vet at det har kommet en ny sak når ikonet har en blå sirkel.'
     }
 ];
 
