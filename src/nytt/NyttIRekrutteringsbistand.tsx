72add80d44865772f0d47fc57b1b7194536a534b
André Mossige <mossige.a@gmail.com>
2020-04-22 15:29:21 +0200

    Legg til nyhet om visning av kandidatens veileder

--- src/nytt/NyttIRekrutteringsbistand.tsx
+++ src/nytt/NyttIRekrutteringsbistand.tsx
@@ -7,10 +7,16 @@ const tilDato = (dag: number, måned: number, år: number, timer: number = 0, mi
 
 const nyheter = [
     {
+        dato: tilDato(22, 4, 2020),
+        tittel: 'Informasjon om kandidatens veileder',
+        innhold:
+            'Nå kan du se hvem som er kandidaten sin veileder fra Rekrutteringsbistand. Informasjon om veileder finner du i CV-visningen på kandidaten.',
+    },
+    {
         dato: tilDato(20, 4, 2020),
         tittel: 'Nå kan du søke etter kandidater som har fersk arbeidserfaring',
         innhold:
-            'I kandidatsøket har det kommet et nytt filter. I filteret legger du inn yrket som kandidatene skal ha arbeidserfaringen innen, og velger hvor fersk arbeidserfaringen skal være. Du kan velge om du vil se kandidater som har arbeidserfaring fra siste to år, siste fem år eller selv velge antall år med fersk arbeidserfaring.'
+            'I kandidatsøket har det kommet et nytt filter. I filteret legger du inn yrket som kandidatene skal ha arbeidserfaringen innen, og velger hvor fersk arbeidserfaringen skal være. Du kan velge om du vil se kandidater som har arbeidserfaring fra siste to år, siste fem år eller selv velge antall år med fersk arbeidserfaring.',
     },
     {
         dato: tilDato(7, 4, 2020),
@@ -33,25 +39,25 @@ const nyheter = [
                     ledig nå, om tre måneder eller etter nærmere avtale.
                 </p>
             </>
-        )
+        ),
     },
     {
         dato: tilDato(6, 4, 2020),
         tittel: 'Nå kan du slette kandidater fra en kandidatliste',
         innhold:
-            'Kandidater som slettes blir arkivert, og du kan enkelt søke frem en oversikt over hvilke kandidater som er slettet. Det er også mulig å angre sletting, og flytte kandidaten tilbake på kandidatlisten.'
+            'Kandidater som slettes blir arkivert, og du kan enkelt søke frem en oversikt over hvilke kandidater som er slettet. Det er også mulig å angre sletting, og flytte kandidaten tilbake på kandidatlisten.',
     },
     {
         dato: tilDato(3, 4, 2020),
         tittel: 'Rekrutteringsbistand på Yammer',
         innhold:
-            'Rekrutteringsbistand har egen gruppe på Yammer. Der kan behov for ny eller endret funksjonalitet i verktøyet meldes inn og bli diskutert. Feil i rekrutteringsbistand skal meldes i porten.'
+            'Rekrutteringsbistand har egen gruppe på Yammer. Der kan behov for ny eller endret funksjonalitet i verktøyet meldes inn og bli diskutert. Feil i rekrutteringsbistand skal meldes i porten.',
     },
     {
         dato: tilDato(2, 4, 2020),
         tittel: 'Ny toppmeny i Rekrutteringsbistand',
         innhold:
-            'I Rekrutteringsbistand er det nå samme dekoratør som i Modia personoversikt. Det vil si at du får tilgang til menyknappen og kan velge enhet. Det gjør det lettere å gå mellom de ulike systemene.'
+            'I Rekrutteringsbistand er det nå samme dekoratør som i Modia personoversikt. Det vil si at du får tilgang til menyknappen og kan velge enhet. Det gjør det lettere å gå mellom de ulike systemene.',
     },
     {
         dato: tilDato(31, 3, 2020),
@@ -68,13 +74,13 @@ const nyheter = [
                     Rekrutteringsbistand for noen utvalgte bransjer.
                 </p>
             </>
-        )
+        ),
     },
     {
         dato: tilDato(31, 3, 2020),
         tittel: 'Kopier lenken til en stilling',
         innhold:
-            'Nederst på stilling under «Om annonsen» kan du nå kopiere lenken til stillingen. Du kan for eksempel sende den til en aktuell kandidat på e-post eller i dialogen slik at kandidaten kan vurdere om stillingen passer.'
+            'Nederst på stilling under «Om annonsen» kan du nå kopiere lenken til stillingen. Du kan for eksempel sende den til en aktuell kandidat på e-post eller i dialogen slik at kandidaten kan vurdere om stillingen passer.',
     },
     {
         dato: tilDato(26, 3, 2020),
@@ -100,26 +106,26 @@ const nyheter = [
                     </a>
                 </p>
             </>
-        )
+        ),
     },
     {
         dato: tilDato(28, 2, 2020),
         tittel: 'Enklere å skrive et notat',
         innhold:
-            'Nå blir det enklere å skrive et notat om hvorfor brukeren passer til stillingen. Når du legger til en kandidat på en kandidatliste, får du mulighet til å opprette et notat.'
+            'Nå blir det enklere å skrive et notat om hvorfor brukeren passer til stillingen. Når du legger til en kandidat på en kandidatliste, får du mulighet til å opprette et notat.',
     },
     {
         dato: tilDato(26, 2, 2020),
         tittel: 'Endring av fornavn og etternavn',
         innhold:
-            'I kandidatsøket vises nå etternavn før fornavn. På e-poster som automatisk sendes til arbeidsgiver har vi også endret rekkefølge. Nå vil fornavnet ditt vises før etternavnet.'
+            'I kandidatsøket vises nå etternavn før fornavn. På e-poster som automatisk sendes til arbeidsgiver har vi også endret rekkefølge. Nå vil fornavnet ditt vises før etternavnet.',
     },
     {
         dato: tilDato(26, 2, 2020),
         tittel: 'Vi har nå lansert «nytt i Rekrutteringsbistand»',
         innhold:
-            'Her vil du få oversikt over nyheter og endringer som gjøres. Du vet at det har kommet en ny sak når ikonet har en blå sirkel.'
-    }
+            'Her vil du få oversikt over nyheter og endringer som gjøres. Du vet at det har kommet en ny sak når ikonet har en blå sirkel.',
+    },
 ];
 
 type Props = Partial<PopoverProps>;
